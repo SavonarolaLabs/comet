7d455cce2aae0121500a6976f0ea2e25b4712fb9
c8e4 <c8e4@proton.me>
2024-12-07 21:55:58 +0100

    hover colors

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -34,7 +34,7 @@
 					aria-label="Twitter"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="transition-colors hover:text-blue-500"
+					class="transition-colors"
 				>
 					<TwitterIcon class="h-6 w-6" />
 				</a>
@@ -43,7 +43,7 @@
 					aria-label="Discord"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="transition-colors hover:text-purple-500"
+					class="transition-colors"
 				>
 					<svg
 						width="26"
@@ -52,15 +52,15 @@
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						><path
-							stroke="white"
+							stroke="currentColor"
 							stroke-linecap="round"
 							stroke-linejoin="round"
 							stroke-width="12"
 							d="m68 138-8 16c-10.19-4.246-20.742-8.492-31.96-15.8-3.912-2.549-6.284-6.88-6.378-11.548-.488-23.964 5.134-48.056 19.369-73.528 1.863-3.334 4.967-5.778 8.567-7.056C58.186 43.02 64.016 40.664 74 39l6 11s6-2 16-2 16 2 16 2l6-11c9.984 1.664 15.814 4.02 24.402 7.068 3.6 1.278 6.704 3.722 8.567 7.056 14.235 25.472 19.857 49.564 19.37 73.528-.095 4.668-2.467 8.999-6.379 11.548-11.218 7.308-21.769 11.554-31.96 15.8l-8-16m-68-8s20 10 40 10 40-10 40-10"
-						/><ellipse cx="71" cy="101" fill="white" rx="13" ry="15" /><ellipse
+						/><ellipse cx="71" cy="101" fill="currentColor" rx="13" ry="15" /><ellipse
 							cx="121"
 							cy="101"
-							fill="white"
+							fill="currentColor"
 							rx="13"
 							ry="15"
 						/></svg
@@ -71,7 +71,7 @@
 					aria-label="GitHub"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="transition-colors hover:text-gray-700"
+					class="transition-colors"
 				>
 					<GithubIcon class="h-6 w-6" />
 				</a>
@@ -80,7 +80,7 @@
 					aria-label="Telegram"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="transition-colors hover:text-blue-400"
+					class="transition-colors"
 				>
 					<SendIcon class="h-6 w-6" />
 				</a>
